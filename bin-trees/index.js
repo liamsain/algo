036801bc5829dc0f6498ecb76675d3d594e72255
liamsain <liamkennedy89@outlook.com>
2021-08-05 22:31:49 +0100

    bin search tree stuff

--- bin-trees/index.js
+++ bin-trees/index.js
@@ -5,7 +5,39 @@ class TreeNode {
     this.right = config.right;
   }
 }
-// a good exercise would be an alg that builds a binary search tree from an array
 const node1 = new TreeNode({value: 20});
 const node2 = new TreeNode({value: 30});
 const root = new TreeNode({value: 25, left: node1, right: node2});
+
+
+function searchForVal(val, node) {
+  if (!node || node.value === val) {
+    return node;
+  } else if (val < node.value) {
+    return searchForVal(val, node.left);
+  } else {
+    return searchForVal(val, node.right);
+  }
+}
+
+const a = searchForVal(11, root);
+console.log(a);
+
+function insertNode(val, node) {
+  if (val < node.value) {
+    if (!node.left) {
+      node.left = new TreeNode({value: val});
+    } else {
+      insertNode(val, node.left);
+    }
+  } else if (val > node.value) {
+    if (!node.right) {
+      node.right = new TreeNode({value: val});
+    } else {
+      insertNode(val, node.right);
+    }
+  }
+}
+insertNode(50, root);
+insertNode(10, root);
+console.log(root);
